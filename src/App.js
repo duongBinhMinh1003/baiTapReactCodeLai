45989c00917d29febf3f755aabc7c638aae9f56c
duongBinhMinh1003 <duongbinhminh10032004@gmail.com>
2023-11-23 18:11:03 +0700

    baiTapReact

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
      
-     {/* <ExShoe></ExShoe> */}
-     <Layout></Layout>
+     <ExShoe></ExShoe>
+     {/* <Layout></Layout> */}
     </div>
   );
 }
